1cb2047ed0d49689a5b8f38153e201b582f13f50
harshithpabbati <pabbatiharshith@gmail.com>
2021-08-26 01:25:24 +0530

    fix: layout auth loading

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Head from 'next/head';
 
 import { useRouter } from 'next/router';
@@ -16,10 +16,12 @@ const defaultProps = {
 
 const Layout = ({ children, title }: Props) => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const user = supabase.auth.user();
 
   useEffect(() => {
     if (!user) router.push('/auth');
+    else setLoading(false);
   }, [router, user]);
 
   return (
@@ -30,7 +32,17 @@ const Layout = ({ children, title }: Props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header />
-      <div style={{ marginTop: '10vh' }}>{children}</div>
+      <div style={{ marginTop: '10vh' }}>
+        {!loading ? (
+          children
+        ) : (
+          <div className="d-flex align-items-center min-vh-100">
+            <div className="container text-center">
+              <h4>Loading</h4>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
